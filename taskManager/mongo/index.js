0a4f26e57bf7fbc3a1ed55e90070c9b377d0fbc2
Tobey <cyberfly@email.cz>
2021-11-30 17:58:03 +0100

    testing inserts

--- taskManager/mongo/index.js
+++ taskManager/mongo/index.js
@@ -10,4 +10,59 @@ MongoClient.connect(url, {
         return console.log('Unable to connect to DB');
     }
     console.log('Connected to MongoDB server');
+
+    client
+        .db(dbName)
+        .collection('users')
+        .insertOne({
+            name: 'Tobey',
+            age: 33
+        }).then(inserted => {
+            client.db(dbName)
+                .collection('users')
+                .findOne({name: 'Tobey'})
+                .then((verified) => {
+                    console.log({inserted, verified});
+                }).catch((error) => {
+                    console.log(error);
+                });
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    client
+        .db(dbName)
+        .collection('tasks')
+        .insertMany([
+            {
+                task: 'Buy milk',
+                done: false,
+            },
+            {
+                task: 'Buy bread',
+                done: false,
+            },
+            {
+                task: 'Buy cheese',
+                done: false,
+            }
+        ])
+        .then((result) => {
+            console.log({result});
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    client
+        .db(dbName)
+        .collection('tasks')
+        .estimatedDocumentCount()
+        .then((count) => {
+            console.log({count});
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
 });
