ea9283adb8106dadbabf87245c7dec90a46ced18
Tobey <cyberfly@email.cz>
2022-01-19 18:42:13 +0100

    separate app for testing

---
+++ taskManager/app/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// requests get stuck w/o this!!!
+const db = require('./db');
+
+const routers = {};
+routers.users = require('./routers/users');
+routers.tasks = require('./routers/tasks');
+
+const app = express();
+
+// middleware to stop exectution with no call to next()
+// app.use((req, res, next) =>
+//     res
+//         .status(503)
+//         .send('Website is under maintenance.'));
+
+// automatic parsing of json
+// must come before router to work!!!
+app.use(express.json());
+
+app.use(routers.users);
+app.use(routers.tasks);
+
+module.exports = app;
--- taskManager/app/index.js
+++ taskManager/app/index.js
@@ -1,26 +1,4 @@
-const express = require('express');
-
-// requests get stuck w/o this!!!
-const db = require('./db');
-
-const routers = {};
-routers.users = require('./routers/users');
-routers.tasks = require('./routers/tasks');
-
-const app = express();
-
-// middleware to stop exectution with no call to next()
-// app.use((req, res, next) =>
-//     res
-//         .status(503)
-//         .send('Website is under maintenance.'));
-
-// automatic parsing of json
-// must come before router to work!!!
-app.use(express.json());
-
-app.use(routers.users);
-app.use(routers.tasks);
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
